ef339be2cb4b4326a064baf66fda199651aed41d
Samuele Maurizi <samuelemaurizi@gmail.com>
2019-09-26 17:40:31 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const connectDB = require('./config/db');
 
@@ -10,14 +11,22 @@ connectDB();
 // Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the contact keeper API' })
-);
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to the contact keeper API' })
+// );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
